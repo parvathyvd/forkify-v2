10aadd27dfa59f8141e99b7d3cea72e258fe6ddd
Parvathy VD <paru@Parvathys-MacBook-Pro.local>
2022-02-11 19:50:17 -0800

    Forkify project version2 after the api update and used parcel instead of webpack

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2b7a1a55-30e7-47ff-b76a-ba973c6700ab';
+export const MODAL_CLOSE_SEC = 2.5;
